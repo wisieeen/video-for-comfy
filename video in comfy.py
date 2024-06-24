2013044af7d0332f61c899704138d0784ff122a8
wisieeen <wisieeen@gmail.com>
2024-06-24 23:48:58 +0300

    full hydrogen video generation data

--- video in comfy.py
+++ video in comfy.py
@@ -21,6 +21,8 @@ generated_image = "ComfyUI_00001_.png"
 #keep in mind ComfyUI is pre alpha software so this format will change a bit.
 
 #this is the one for the default workflow
+
+#prompt for ipadapter interpolation
 prompt_text_interpolation = """
 {
   "3": {
@@ -303,6 +305,8 @@ prompt_text_interpolation = """
   }
 }
 """
+
+#it infinitely zooms in chosen direction
 prompt_text_zoom = """{
   "3": {
     "inputs": {
@@ -468,17 +472,219 @@ prompt_text_zoom = """{
   }
 }"""
 
+#it interpolates between prompts and zooms
+prompt_interpolation = """
+{
+  "3": {
+    "inputs": {
+      "seed": 173159221580066,
+      "steps": 4,
+      "cfg": 2,
+      "sampler_name": "euler_ancestral",
+      "scheduler": "sgm_uniform",
+      "denoise": 0.45,
+      "model": [
+        "4",
+        0
+      ],
+      "positive": [
+        "21",
+        0
+      ],
+      "negative": [
+        "7",
+        0
+      ],
+      "latent_image": [
+        "12",
+        0
+      ]
+    },
+    "class_type": "KSampler",
+    "_meta": {
+      "title": "KSampler"
+    }
+  },
+  "4": {
+    "inputs": {
+      "ckpt_name": "dreamshaperXL_lightningDPMSDE.safetensors"
+    },
+    "class_type": "CheckpointLoaderSimple",
+    "_meta": {
+      "title": "Load Checkpoint"
+    }
+  },
+  "6": {
+    "inputs": {
+      "text": "diamond in coal wall under high pressure and temperature deep underground. 3d render",
+      "clip": [
+        "4",
+        1
+      ]
+    },
+    "class_type": "CLIPTextEncode",
+    "_meta": {
+      "title": "CLIP Text Encode (Prompt)"
+    }
+  },
+  "7": {
+    "inputs": {
+      "text": "text, watermark",
+      "clip": [
+        "4",
+        1
+      ]
+    },
+    "class_type": "CLIPTextEncode",
+    "_meta": {
+      "title": "CLIP Text Encode (Prompt)"
+    }
+  },
+  "8": {
+    "inputs": {
+      "samples": [
+        "3",
+        0
+      ],
+      "vae": [
+        "4",
+        2
+      ]
+    },
+    "class_type": "VAEDecode",
+    "_meta": {
+      "title": "VAE Decode"
+    }
+  },
+  "10": {
+    "inputs": {
+      "images": [
+        "8",
+        0
+      ]
+    },
+    "class_type": "PreviewImage",
+    "_meta": {
+      "title": "Preview Image"
+    }
+  },
+  "12": {
+    "inputs": {
+      "pixels": [
+        "13",
+        0
+      ],
+      "vae": [
+        "4",
+        2
+      ]
+    },
+    "class_type": "VAEEncode",
+    "_meta": {
+      "title": "VAE Encode"
+    }
+  },
+  "13": {
+    "inputs": {
+      "width": 1344,
+      "height": 768,
+      "x": 20,
+      "y": 11,
+      "image": [
+        "14",
+        0
+      ]
+    },
+    "class_type": "ImageCrop",
+    "_meta": {
+      "title": "ImageCrop"
+    }
+  },
+  "14": {
+    "inputs": {
+      "upscale_method": "nearest-exact",
+      "scale_by": 1.03,
+      "image": [
+        "15",
+        0
+      ]
+    },
+    "class_type": "ImageScaleBy",
+    "_meta": {
+      "title": "Upscale Image By"
+    }
+  },
+  "15": {
+    "inputs": {
+      "image": "processed.png",
+      "upload": "image"
+    },
+    "class_type": "LoadImage",
+    "_meta": {
+      "title": "Load Image"
+    }
+  },
+  "18": {
+    "inputs": {
+      "filename_prefix": "ComfyUI",
+      "images": [
+        "8",
+        0
+      ]
+    },
+    "class_type": "SaveImage",
+    "_meta": {
+      "title": "Save Image"
+    }
+  },
+  "19": {
+    "inputs": {
+      "text": "diamond in saw blade disc cutting toughest materials. 3d render",
+      "clip": [
+        "4",
+        1
+      ]
+    },
+    "class_type": "CLIPTextEncode",
+    "_meta": {
+      "title": "CLIP Text Encode (Prompt)"
+    }
+  },
+  "21": {
+    "inputs": {
+      "conditioning_to_strength": 0,
+      "conditioning_to": [
+        "19",
+        0
+      ],
+      "conditioning_from": [
+        "6",
+        0
+      ]
+    },
+    "class_type": "ConditioningAverage",
+    "_meta": {
+      "title": "ConditioningAverage"
+    }
+  }
+}
+"""
 
 
 def copy_and_rename_image(source_name, destination_directory, new_name):
-    """Copies an image file from source to destination and renames it."""
-    time.sleep(.2)
+    """Copies an image file from source to destination and renames it.
+    @source_name: path to image to be copied
+    @destination_directory: folder path of destination
+    @new_name: final name of copied file. include desired extension
+    """
+    time.sleep(.2) #without sleep file may be processed while saving or coping not finished and break script
     d = copy2(source_name, destination_directory)
     os.unlink('E:\\ComfyUI_windows\\ComfyUI\\input\\processed.png')
     time.sleep(.2)
     os.rename(d, destination_directory + new_name)
 
 def queue_prompt(prompt):
+    """sends prompt to comfyUI"""
     p = {"prompt": prompt}
     data = json.dumps(p).encode('utf-8')
     req =  request.Request("http://127.0.0.1:8188/prompt", data=data)
@@ -488,7 +694,7 @@ def image_watch(latest_file):
     '''
     function that waits until new file appears in folder
     returns name ot this file
-    @latest_file: file that was added
+    @latest_file: last generated file
     '''
     maybe_latest_file = latest_file
 
@@ -522,21 +728,75 @@ def image_interpolation(steps, start, end, image1, image2, image_to_be_processed
         image_to_be_processed = image_watch(image_to_be_processed)
         print(image_to_be_processed)
 
-def image_zoom():
+def image_zoom(steps, scale_by,prompt_text, x = 20,y = 11):
+    """zooms in one particular direction
+    @steps: how many steps of this """
     global generated_image
     prompt = json.loads(prompt_text_zoom) 
     prompt["3"]["inputs"]["denoise"] = denoise
     prompt["15"]["inputs"]["image"] = "processed.png"
     prompt["6"]["inputs"]["text"] = prompt_text
-    prompt["13"]["inputs"]["x"] = 20  #34 for centered zoom at 1.05 and [1344,768]
-    prompt["13"]["inputs"]["y"] = 11  #19 for centered zoom
-    prompt["14"]["inputs"]["scale_by"] = 1.03
-    while(True):
+    prompt["13"]["inputs"]["x"] = x  #34 for centered zoom at 1.05 and [1344,768]
+    prompt["13"]["inputs"]["y"] = y  #19 for centered zoom
+    prompt["14"]["inputs"]["scale_by"] = scale_by
+    steps_done = 0
+    while(steps>steps_done):
+        steps_done +=1
         prompt["3"]["inputs"]["seed"] += 1 
         queue_prompt(prompt)
         generated_image = image_watch(generated_image)
         copy_and_rename_image(generated_image, path_to_load_image, "processed.png")
 
+def image_zoom_n_interpolation(steps, scale_by,prompt_text_start, prompt_text_end, x = 20,y = 11):
+    """zooms in one particular direction
+    @steps: how many steps of this stransition
+    @scale_by: scale factor
+    @prompt_text_start: prompt to start with
+    @prompt_text_end: prompt to end with
+    @x: cut pixels to left
+    @y: cut pixels from top """
+
+    global generated_image
+    prompt = json.loads(prompt_interpolation) 
+    prompt["3"]["inputs"]["denoise"] = denoise
+    prompt["15"]["inputs"]["image"] = "processed.png"
+    prompt["6"]["inputs"]["text"] = prompt_text_start
+    prompt["19"]["inputs"]["text"] = prompt_text_end
+    prompt["13"]["inputs"]["x"] = x  #34 for centered zoom at 1.05 and [1344,768]
+    prompt["13"]["inputs"]["y"] = y  #19 for centered zoom
+    prompt["14"]["inputs"]["scale_by"] = scale_by
+    steps_done = 0
+    while(steps>steps_done):
+        steps_done +=1
+        prompt["3"]["inputs"]["seed"] += 1 
+        prompt["21"]["inputs"]["conditioning_to_strength"] = steps_done/steps
+        queue_prompt(prompt)
+        generated_image = image_watch(generated_image)
+        copy_and_rename_image(generated_image, path_to_load_image, "processed.png")
+
+
 denoise = 0.45 #denoise of latent image
-prompt_text = "cyborg tiger in cyberpunk city. highly detailed analog photography."
-image_zoom()
+prompt_text = "hydrogen atom. 3d render"
+generated_image = image_watch
+image_zoom(99, 1.03, prompt_text)
+p1 = prompt_text
+p2 = "steel reactor for hydrogenation. 3d render"
+image_zoom_n_interpolation(180,1.03,p1,p2)
+
+image_zoom(300, 1.03, p2)
+
+p1 = p2
+p2 = "large sun. closeup telescope image"
+image_zoom_n_interpolation(180,1.03,p1,p2)
+
+image_zoom(320, 1.03, p2)
+
+p1 = p2
+p2 = "explosion in empty space"
+image_zoom_n_interpolation(120,1.03,p1,p2)
+
+p1 = p2
+p2 = "water sphere. 3d render"
+image_zoom_n_interpolation(120,1.03,p1,p2)
+
+image_zoom(320, 1.03, p2)
